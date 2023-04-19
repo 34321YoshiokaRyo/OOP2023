f34b94c8646eb322642e9164564c008e89959ca5
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-04-19 11:56:13 +0900

    複数のボール

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -12,7 +12,10 @@ namespace BallApp {
         private Timer moveTimer;    //タイマー用
         private SoccerBall soccerBall;
         private PictureBox pb;
-        
+
+        private List<SoccerBall> balls = new List<SoccerBall>();    //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>();      //表示用
+
         static void Main(string[] args) {
             Application.Run(new Program());
 
@@ -24,8 +27,6 @@ namespace BallApp {
             this.Text = "BallGame";
             this.MouseClick += Program_MouseClick;
 
-            //ボールインスタンス生成
-            
             moveTimer = new Timer();
             moveTimer.Interval = 30;//タイマーのインターバル
            
@@ -34,21 +35,31 @@ namespace BallApp {
 
         //マウスクリック時のイベントハンドラ
         private void Program_MouseClick(object sender, MouseEventArgs e) {
-            pb = new PictureBox();   //画像を表示するコントロール
+            //ボールインスタンス生成
             soccerBall = new SoccerBall(e.X-25, e.Y-25);
+            pb = new PictureBox();   //画像を表示するコントロール
             pb.Image = soccerBall.Image;
             pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);//画像の位置
             pb.Size = new Size(50, 50);//画像の表示サイズ
             pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
             pb.Parent = this;
 
+            balls.Add(soccerBall);
+            pbs.Add(pb);
+
             moveTimer.Start();//タイマースタート
         }
 
         //タイマーアウト時のイベントハンドラ
         private void MoveTimer_Tick(object sender, EventArgs e) {
-            soccerBall.Move(); //移動
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY); //画像の位置
+
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].Move(); //移動
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY); //画像の位置
+            }
+           
         }
     }
+   
 }
