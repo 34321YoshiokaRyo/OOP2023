9dfdcf199294dbe141ada3d224fd60f0c5e54031
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-05-08 10:54:11 +0900

    p26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    
+    class Program {
+        static void Main(string[] args) {
+           //インスタンスの作成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(235, "大福もち", 160);
+            
+            Console.WriteLine(karinto.GetPriceIncludingTax());
+            Console.WriteLine(daifuku .GetPriceIncludingTax());
+        }
+    }
+}
