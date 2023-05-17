782a441633e7a84dab4c561c2611e12b9980f661
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-05-17 11:22:00 +0900

    p.79問題2.1完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+
+            //2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            PrintSongs(songs);
+
+            var lists = new List<Song>();
+            lists.Add(new Song("Let it be", "The Beatles", 243));
+            lists.Add(new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293));
+            lists.Add(new Song("Close To You", "Carpenters", 276));
+            lists.Add(new Song("Honesty", "Billy Joel", 231));
+            lists.Add(new Song("I Will Always Love You", "Whitney Houston", 273));
+
+            PrintSongs(lists);
+
+        }
+
+        private static void PrintSongs(IEnumerable<Song> songs) {
+            foreach (var song in songs) {
+                Console.WriteLine("{0}, {1}, {2:m\\:ss}", song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
         }
     }
 }
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Song {
+        //自動実装プロパティ
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        public Song(string Title, string ArtistName, int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+
+    }
+}
