b002467ce18b79891e1f87a6e3d8404b6d9e8c76
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-05-22 09:54:35 +0900

    p79問題2.3完成

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var sales = new SalesCounter((@"data\sales.csv"));
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            }
+
+            Console.WriteLine();
+
+            var amountPerCategory = sales.GetPerCategory();
+            foreach (var obj in amountPerCategory) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            }
+
         }
     }
 }
---
+++ Chapter02/演習問題/Exercise03/SalesCounter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    public class SalesCounter 
+    {
+        private IEnumerable<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(string filePath) {
+             _sales = ReadSales(filePath);
+        }
+
+        //店舗別売り上げを求める
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;    //店名が既に存在する（売上加算）
+                else
+                    dict[sale.ShopName] = sale.Amount;   //店名が存在しない（新規格納）
+            }
+            return dict;
+        }
+
+        public IDictionary<string, int> GetPerCategory() {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ProductCategory] += sale.Amount;
+                else
+                    dict[sale.ProductCategory] = sale.Amount;
+            }
+            return dict;
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        private static IEnumerable<Sale> ReadSales(string filePath) {
+            var sales = new List<Sale>();//売上データを格納する
+            var lines = File.ReadAllLines(filePath);//ファイルからすべてのデータを読み込む
+
+            foreach (var line in lines) //すべての行から一行ずつ取り出す
+            {
+                var items = line.Split(',');//区切りで項目別に分ける
+                var sale = new Sale    // Saleインスタンスを生成 
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);    //Saleインスタンスをコレクションに追加
+            }
+
+            return sales;
+        }
+    }
+}
