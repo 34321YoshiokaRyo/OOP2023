07a4e53245cdab22c2b6c2d752b25f215797899f
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-05-23 09:25:11 +0900

    p87 Step5まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -10,20 +10,9 @@ namespace Section01 {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            int count = Count(numbers, delegate(int n) { return n % 2 == 0; });
+            int count = numbers.Count( n => n % 2 == 0);
+
             Console.WriteLine(count);
-            }
-
-        static public int Count(int[] numbers, Predicate<int> judge) {
-           int count = 0;
-
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count += 1;
-                }
-            }
-            return count;
-        } 
-
+        }
     }
 }
