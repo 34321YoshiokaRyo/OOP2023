ab094e1629a734b3f897d460f813319d99f39c89
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-05-23 10:30:41 +0900

    p87 まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,11 +8,15 @@ namespace Section01 {
     class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4};
 
-            int count = numbers.Count( n => n % 2 == 0);
+            //５の倍数をカウントする
+            //int count = numbers.Count( n => n % 5 == 0 && n > 0);
 
-            Console.WriteLine(count);
+            //合計値
+            var sum = numbers.Where(n => n % 2 == 0).Sum();
+
+            Console.WriteLine(sum);
         }
     }
 }
