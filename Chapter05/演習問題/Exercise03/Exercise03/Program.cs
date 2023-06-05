5be50c027fabc691aa2e9bb8bf7a66e480c0cf87
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-06-05 10:40:54 +0900

    5.3.5

--- Chapter05/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Exercise03/Program.cs
@@ -51,7 +51,14 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_5(string text) {
-           
+            var words = text.Split(' ');
+            var sb = new StringBuilder();
+            foreach (var item in words) {
+                sb.Append(item);
+                sb.Append(" ");
+            }
+            var str = sb.ToString();
+            Console.WriteLine(str);
         }
     }
 }
