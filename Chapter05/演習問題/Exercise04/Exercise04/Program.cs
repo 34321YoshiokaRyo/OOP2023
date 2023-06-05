7a922a10ba2e5dcfb8f229e7cd746f0ea6fe5d9d
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-06-05 12:09:20 +0900

    演習問題5.4新規作成

---
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+        }
+    }
+}
