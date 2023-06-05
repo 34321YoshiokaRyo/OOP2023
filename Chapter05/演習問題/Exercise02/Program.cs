45ab119d29e43197e66b8eb1e511a971abae18a3
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-06-05 09:30:49 +0900

    演習問題5.1 5.2完成 5.3作成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("数字文字列：");
+            var line = Console.ReadLine();
+            int num;
+
+            if (int.TryParse(line,out num)) {
+                var str = string.Format("{0:#,0}", num);
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
