634940a96dc233c59246cc0b9d0c8023162781a2
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-06-06 09:47:22 +0900

    演習問題5.4

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -8,6 +8,12 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var replaced = line.Replace("Novelist", "作家").Replace("BestWork","代表作").Replace("Born","誕生年");
+            var words = replaced.Split(';','=');
+
+            Console.WriteLine(words[0] + "  : " + words[1]);
+            Console.WriteLine(words[2] + ": " + words[3]);
+            Console.WriteLine(words[4] + ": " + words[5]);
         }
     }
 }
