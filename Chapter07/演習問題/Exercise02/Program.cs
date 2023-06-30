384fc195b6c5bd86ea5e81b676e3d39ca1e99d04
吉岡　亮 <OIBC34321@ojs.ac.jp>
2023-06-30 10:27:29 +0900

    7.2

---
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Abbreviations a = new Abbreviations();
+            Console.WriteLine(a.Count);
+
+            Console.Write("省略語：");
+            var str = Console.ReadLine();
+            a.Remove(str);
+            Console.WriteLine(a.Count);
+
+            a.Return();
+        }
+    }
+}
